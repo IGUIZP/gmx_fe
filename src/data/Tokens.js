447fc8e1a68a8bef058cd90a340706cc04604d0e
Rock Gui <suihexinde@163.com>
2022-03-18 19:57:34 +0800

    update address config

--- src/data/Tokens.js
+++ src/data/Tokens.js
@@ -15,7 +15,7 @@ const TOKENS = {
       name: "WGT Token",
       symbol: "WGT",
       decimals: 18,
-      address: "0x09A9137b4707CA685829A5372F0aB7987A70EBCd",
+      address: "0xf57B8AE8A5218540b45a2cba6A651A3dC4f7e6a0",
       isWrapped: true,
       imageUrl:
         "https://assets.coingecko.com/coins/images/279/small/ethereum.png?1595348880"
@@ -24,7 +24,7 @@ const TOKENS = {
       name: "ETH Token",
       symbol: "ETH",
       decimals: 18,
-      address: "0x7b94d9EeFddd440130Af72bcD02F3ad394b60eb1",
+      address: "0xB9a0e7ec640120d207ffE1Fc35611828D6596D79",
       isShortable: true,
       imageUrl:
         "https://assets.coingecko.com/coins/images/279/small/ethereum.png?1595348880"
@@ -33,7 +33,7 @@ const TOKENS = {
       name: "BTC Token",
       symbol: "BTC",
       decimals: 18,
-      address: "0x7Abb60B462718FdAd5Bb4fb6D78f1048EB0E00D0",
+      address: "0x71725C9382188f11E8D1d61145Bfda27367Bc26F",
       isShortable: true,
       imageUrl:
         "https://assets.coingecko.com/coins/images/7598/thumb/wrapped_bitcoin_wbtc.png?1548822744"
@@ -42,7 +42,7 @@ const TOKENS = {
       name: "BNB Token",
       symbol: "BNB",
       decimals: 18,
-      address: "0xCf4374E64BB2EeC34dD38F0b1Ba7e01f3aca093d",
+      address: "0x196EBEd51369390Ef2945Aecd0343c5496f7290a",
       isWrapped: true,
       imageUrl:
         "https://s2.coinmarketcap.com/static/img/coins/64x64/1839.png"
@@ -51,7 +51,7 @@ const TOKENS = {
       name: "DAI Token",
       symbol: "DAI",
       decimals: 18,
-      address: "0x0836C5E085E512D41160C4B36a634cD87e0b69aA",
+      address: "0x2bBa033B205C93ee37A72De2836ccB010f609844",
       isWrapped: true,
       isStable: true,
       imageUrl:
